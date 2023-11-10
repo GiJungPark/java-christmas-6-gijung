4eb085f8d23c366bf634084918ed12afbde30ba4
GiJung <weeds2577@gmail.com>
2023-11-10 17:32:49 +0900

    feat(OutputView): 시작 메세지 출력 기능 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,13 @@
 package christmas;
 
+import christmas.view.OutputView;
+
 public class Application {
+
+    private static OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        outputView.printStart();
     }
 }
---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,14 @@
+package christmas.view;
+
+public class OutputView {
+
+    public void printStart() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void printMenu() {
+        System.out.println("<주문 메뉴>");
+        // ...
+    }
+    // ...
+}
