54fc750ecd0eda733b6edb63781dcb4295c49beb
GiJung <weeds2577@gmail.com>
2023-11-10 19:16:59 +0900

    feat(VisitDate): 방문 날짜 객체 생성, 방문 날짜 객체의 예외 구현

---
+++ src/main/java/christmas/domain/VisitDate.java
@@ -0,0 +1,20 @@
+package christmas.domain;
+
+public class VisitDate {
+
+    private final int number;
+
+    public VisitDate(int number) {
+
+        validateNumberSize(number);
+
+        this.number = number;
+    }
+
+    private void validateNumberSize(int number) {
+        if(1 > number || number > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+}
