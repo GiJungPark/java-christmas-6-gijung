5113afe4b33a64389c9540acca0b51cfa83db0b2
GiJung <weeds2577@gmail.com>
2023-11-12 00:50:34 +0900

    test(VisitDateTest): 정상 작동 테스트 케이스 추가

--- src/test/java/christmas/domain/VisitDateTest.java
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -1,12 +1,20 @@
 package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class VisitDateTest {
+
+    @DisplayName("정상적인 방문 날짜 처리")
+    @ValueSource(strings = {"1", "10", "20", "31"})
+    @ParameterizedTest
+    void qwe(int visitDateNumber) {
+        assertThat(new VisitDate(visitDateNumber)).isInstanceOf(VisitDate.class);
+    }
 
     @DisplayName("1 ~ 31 이외의 방문 날짜에 대한 예외 처리")
     @ValueSource(strings = {"-1", "0", "32"})
