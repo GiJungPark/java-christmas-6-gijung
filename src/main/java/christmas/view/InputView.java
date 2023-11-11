9b1498be81dee651b2ddaf7006ddedd7d39381df
GiJung <weeds2577@gmail.com>
2023-11-12 01:23:48 +0900

    feat(InputView): 주문할 메뉴와 개수 입력, 입력에서의 예외 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,6 +2,9 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class InputView {
     public int readDate() {
 
@@ -35,5 +38,67 @@ public class InputView {
         }
     }
 
+    public HashMap<String, Integer> readMenu() {
+
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+
+        String input = readLineByRemovingSpaces();
+
+        validateNull(input);
+
+        return separate(input);
+    }
+
+    private void validateKorean(String input) {
+        if (!input.matches("^[ㄱ-ㅎ가-힣]*$")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private HashMap<String, Integer> separate(String input) {
+
+        HashMap<String, Integer> separatedInput = new HashMap<>();
+        for (String inputLine : separateWithComma(input)) {
+            List<String> inputValues = separateWithHyphen(inputLine);
+
+            validateKorean(inputValues.get(0));
+            validateNumber(inputValues.get(1));
+            duplicateMenuTitle(separatedInput, inputValues.get(0));
+
+            separatedInput.put(inputValues.get(0), Integer.parseInt(inputValues.get(1)));
+        }
+
+        return separatedInput;
+    }
+
+    private List<String> separateWithComma(String input) {
+
+        List<String> separatedInput = List.of(input.split(","));
+
+        validateSeparateSize(separatedInput.size());
+
+        return separatedInput;
+    }
+
+    private List<String> separateWithHyphen(String input) {
+
+        List<String> separatedInput = List.of(input.split("-"));
+
+        validateSeparateSize(separatedInput.size());
+
+        return separatedInput;
+    }
+
+    private void validateSeparateSize(int size) {
+        if (size != 2) {
+            throw new IllegalArgumentException("입력값 형식에 맞지 않는다.");
+        }
+    }
+
+    private void duplicateMenuTitle(HashMap<String, Integer> separatedInput, String menuTitle) {
+        if (separatedInput.containsKey(menuTitle)) {
+            throw new IllegalArgumentException("중복");
+        }
+    }
     // ...
 }
