065826e838f2f66ffe3250e98f64dbc6b6addaf4
GiJung <weeds2577@gmail.com>
2023-11-12 01:26:56 +0900

    feat(OrderMenus): 주문한 메뉴와 개수의 객체 생성, 예외 구현

---
+++ src/main/java/christmas/domain/OrderMenus.java
@@ -0,0 +1,37 @@
+package christmas.domain;
+
+import christmas.domain.configuration.Menu;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class OrderMenus {
+
+    private final HashMap<String, Integer> orderMenus;
+
+    public OrderMenus(HashMap<String, Integer> menus) {
+
+        int totalCount = 0;
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String menu : menus.keySet()) {
+            validateMenuCount(menus.get(menu));
+            totalCount += menus.get(menu);
+            map.put(Menu.convertorTitle(menu), menus.get(menu));
+        }
+
+        validateMenuCount(totalCount);
+
+        Menu.isOnlyDrink(new ArrayList<>(map.keySet()));
+
+        this.orderMenus = map;
+    }
+
+    private void validateMenuCount(int count) {
+        if (count < 1 || count > 20) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+}
