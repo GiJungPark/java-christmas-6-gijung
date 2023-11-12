c61a6ec4f036425c38d855c00fadf1e2da36b335
GiJung <weeds2577@gmail.com>
2023-11-13 01:37:07 +0900

    fix(InputView): 방문 날짜 입력 에러 메세지 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -34,7 +34,7 @@ public class InputView {
 
     private void validateDate(String input) {
         if (!input.matches("\\d*")) {
-            throw new IllegalArgumentException("[ERROR] 날짜는 숫자로 입력해야 합니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
 
