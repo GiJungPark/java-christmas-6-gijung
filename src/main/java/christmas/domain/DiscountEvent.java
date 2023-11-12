056829ee7fec5711cb27420fb2a8205428650777
GiJung <weeds2577@gmail.com>
2023-11-13 02:00:52 +0900

    feat(DiscountEvent): 총 혜택 금액을 계산하는 로직 추가

--- src/main/java/christmas/domain/DiscountEvent.java
+++ src/main/java/christmas/domain/DiscountEvent.java
@@ -4,13 +4,24 @@ import java.util.Map;
 
 public class DiscountEvent {
 
-    private final Map<String, Integer> DiscountEventHistory;
+    private final Map<String, Integer> discountEventHistory;
 
     public DiscountEvent (Map<String, Integer> discountEventHistory) {
-        DiscountEventHistory = discountEventHistory;
+        this.discountEventHistory = discountEventHistory;
     }
 
     public Map<String, Integer> getDiscountEventHistory() {
-        return DiscountEventHistory;
+        return discountEventHistory;
+    }
+
+    public int getTotalDiscountPrice() {
+
+        int totalDiscountPrice = 0;
+
+        for ( String title : discountEventHistory.keySet()) {
+            totalDiscountPrice += discountEventHistory.get(title);
+        }
+
+        return totalDiscountPrice;
     }
 }
