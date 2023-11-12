177564aee287c0cb0c06a08cbc3e44ddca7a9054
GiJung <weeds2577@gmail.com>
2023-11-13 01:32:20 +0900

    feat(ChristmasService): 크리스마스 디데이, 평일, 주말, 특별 할인 계산하는 로직 추가

--- src/main/java/christmas/service/ChristmasService.java
+++ src/main/java/christmas/service/ChristmasService.java
@@ -1,5 +1,6 @@
 package christmas.service;
 
+import christmas.domain.DiscountEvent;
 import christmas.domain.GiveAway;
 import christmas.domain.OrderMenus;
 import christmas.domain.Price;
@@ -7,6 +8,7 @@ import christmas.domain.VisitDate;
 import christmas.configuration.Menu;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ChristmasService {
 
@@ -14,6 +16,8 @@ public class ChristmasService {
     private OrderMenus orderMenu;
     private Price price;
     private GiveAway giveAway;
+    private DiscountEvent discountEvent;
+
 
     public void setVisitDate(int number) {
         this.visitDate = new VisitDate(number);
@@ -50,5 +54,83 @@ public class ChristmasService {
 
     public GiveAway getGiveAwayEvent() {
         return giveAway;
+    }
+
+    public void setDiscountEvent() {
+
+        Map<String, Integer> map = new HashMap<>();
+        if (daysUntilChristmasDiscount() != 0) {
+            map.put("크리스마스 데이 할인", daysUntilChristmasDiscount());
+        }
+        if (weekdayDiscount() != 0 ) {
+            map.put("평일 할인", weekdayDiscount());
+        }
+        if(weekendDiscount() != 0) {
+            map.put("주말 할인", weekendDiscount());
+        }
+        if(specialDiscount() != 0) {
+            map.put("특별 할인", specialDiscount());
+        }
+        if (giveAway.getCount() != 0) {
+            map.put("증정 이벤트", Menu.CHAMPAGNE.getPrice());
+        }
+
+        discountEvent = new DiscountEvent(map);
+    }
+
+    private int daysUntilChristmasDiscount() {
+
+        int price = 0;
+
+        if (visitDate.getNumber() <= 25) {
+            price = 1000 + (visitDate.getNumber() - 1) * 100;
+        }
+
+        return price;
+    }
+
+    private int weekdayDiscount() {
+
+        int price = 0;
+
+        if (visitDate.getNumber() % 7 != 1 && visitDate.getNumber() % 7 != 2) {
+            for (String title : orderMenu.get().keySet()) {
+                if (Menu.isDesert(title)) {
+                    price += orderMenu.get().get(title) * 2023;
+                }
+            }
+        }
+
+        return price;
+    }
+
+    private int weekendDiscount() {
+
+        int price = 0;
+
+        if (visitDate.getNumber() % 7 == 1 || visitDate.getNumber() % 7 == 2) {
+            for (String title : orderMenu.get().keySet()) {
+                if (Menu.isMain(title)) {
+                    price += orderMenu.get().get(title) * 2023;
+                }
+            }
+        }
+
+        return price;
+    }
+
+    private int specialDiscount() {
+
+        int price = 0;
+
+        if (visitDate.getNumber() % 7 == 3 || visitDate.getNumber() == 25) {
+            price = 1000;
+        }
+
+        return price;
+    }
+
+    public DiscountEvent getDiscountEvent() {
+        return discountEvent;
     }
 }
