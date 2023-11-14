ea5f546ae3bd3e5d6569ee0adff951e89950bdca
GiJung <weeds2577@gmail.com>
2023-11-15 00:17:00 +0900

    refactor(GiveAway): 상수만 변경하면 바뀔수 있도록 유지보수에 유리하게 수정

--- src/main/java/christmas/domain/GiveAway.java
+++ src/main/java/christmas/domain/GiveAway.java
@@ -4,19 +4,24 @@ import christmas.configuration.Menu;
 
 public class GiveAway {
 
+    private static final String GIVE_AWAY_TITLE = Menu.CHAMPAGNE.getTitle();
+    private static final int GIVE_AWAY_COUNT = 1;
+    private static final String EMPTY_TITLE = Menu.EMPTY.getTitle();
+    private static final int EMPTY_COUNT = 0;
+
     private final String title;
     private final int count;
 
     public GiveAway(boolean giveAway) {
 
         if (giveAway) {
-            title = Menu.CHAMPAGNE.getTitle();
-            count = 1;
+            title = GIVE_AWAY_TITLE;
+            count = GIVE_AWAY_COUNT;
             return;
         }
 
-        title = Menu.EMPTY.getTitle();
-        count = 0;
+        title = EMPTY_TITLE;
+        count = EMPTY_COUNT;
     }
 
     public String getTitle() {
