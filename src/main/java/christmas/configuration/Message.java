c173cbbeb5ad1c3e2fd699e412176af02651e818
GiJung <weeds2577@gmail.com>
2023-11-14 13:40:16 +0900

    refactor(Message): 상수 이름을 더 알맞게 변경

--- src/main/java/christmas/configuration/Message.java
+++ src/main/java/christmas/configuration/Message.java
@@ -5,7 +5,7 @@ public enum Message {
     START("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
     READ_VISIT_DATE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
     WRITE_VISIT_DATE("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
-    READ_MENU("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
+    READ_ORDER_MENU("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
 
     ORDER_MENU("<주문 메뉴>"),
     TOTAL_PRICE_BEFORE_DISCOUNT("<할인 전 총주문 금액>"),
