d3080d91687d6146e9ff310d965289042d1798e0
GiJung <weeds2577@gmail.com>
2023-11-15 17:54:08 +0900

    refactor(InputView): ErrorMessage를 사용하도록 변경 및 정규식 상수화

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,12 +1,17 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.configuration.ErrorMessage;
 import christmas.configuration.Message;
 
 import java.util.HashMap;
 import java.util.List;
 
 public class InputView {
+
+    private static final String SPACES = " ";
+    private static final String EMPTY = "";
+
     public static String readDate() {
 
         String input = readLineByRemovingSpaces();
@@ -29,75 +34,12 @@ public class InputView {
 
         String input = Console.readLine();
 
-        return input.replaceAll(" ", "");
+        return input.replaceAll(SPACES, EMPTY);
     }
 
     private static void validateNull(String input) {
         if (input.isEmpty()) {
-            throw new IllegalArgumentException("[ERROR] 입력하지 않았습니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(ErrorMessage.INPUT_NULL.getMessage());
         }
     }
-
-//    private static void validateEndWithComma(String input) {
-//        if (input.charAt(input.length() - 1) == ',') {
-//            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-//        }
-//    }
-//
-//    private static HashMap<String, Integer> separate(String input) {
-//
-//        HashMap<String, Integer> separatedInput = new HashMap<>();
-//        for (String inputLine : separateWithComma(input)) {
-//            List<String> inputValues = separateWithHyphen(inputLine);
-//
-//            validateKorean(inputValues.get(0));
-//            validateNumber(inputValues.get(1));
-//            duplicateMenuTitle(separatedInput, inputValues.get(0));
-//
-//            separatedInput.put(inputValues.get(0), Integer.parseInt(inputValues.get(1)));
-//        }
-//
-//        return separatedInput;
-//    }
-//
-//    private static void validateKorean(String input) {
-//        if (!input.matches("^[ㄱ-ㅎ가-힣]*$")) {
-//            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-//        }
-//    }
-//
-//    private static void validateNumber(String input) {
-//        if (!input.matches("\\d*")) {
-//            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-//        }
-//    }
-//
-//    private static List<String> separateWithComma(String input) {
-//
-//        List<String> separatedInput = List.of(input.split(","));
-//
-//        return separatedInput;
-//    }
-//
-//    private static List<String> separateWithHyphen(String input) {
-//
-//        List<String> separatedInput = List.of(input.split("-"));
-//
-//        validateSeparateSize(separatedInput.size());
-//
-//        return separatedInput;
-//    }
-//
-//    private static void validateSeparateSize(int size) {
-//        if (size != 2) {
-//            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-//        }
-//    }
-//
-//    private static void duplicateMenuTitle(HashMap<String, Integer> separatedInput, String menuTitle) {
-//        if (separatedInput.containsKey(menuTitle)) {
-//            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-//        }
-//    }
-    // ...
 }
