dc797febbaa6240c8fea84b32918f4b2ada02af9
GiJung <weeds2577@gmail.com>
2023-11-15 17:56:30 +0900

    refactor(PriceService): 총 금액과 관련된 객체들을 처리하도록 수정

--- src/main/java/christmas/service/PriceService.java
+++ src/main/java/christmas/service/PriceService.java
@@ -1,12 +1,17 @@
 package christmas.service;
 
+import christmas.configuration.Event;
 import christmas.configuration.Menu;
+import christmas.domain.DiscountEvent;
 import christmas.domain.OrderMenus;
 import christmas.domain.Price;
 
 public class PriceService {
 
     private Price totalPriceBeforeDiscount;
+    private Price totalDiscountPrice;
+    private Price discountPrice;
+    private Price expectedPaymentPrice;
 
     public void setTotalPriceBeforeDiscount(OrderMenus orderMenus) {
 
@@ -15,10 +20,51 @@ public class PriceService {
             price += Menu.getPrice(menuTitle) * orderMenus.getValues().get(menuTitle);
         }
 
-        totalPriceBeforeDiscount = new Price(price);
+        this.totalPriceBeforeDiscount = new Price(price);
+    }
+
+    public void setDiscount(DiscountEvent discountEvent) {
+        setTotalDiscountPrice(discountEvent);
+        setDiscountPrice(discountEvent);
+    }
+    public void setTotalDiscountPrice(DiscountEvent discountEvent) {
+
+        int price = 0;
+
+        for (String title : discountEvent.getDiscountEventHistory().keySet()) {
+            price += discountEvent.getDiscountEventHistory().get(title);
+        }
+
+        this.totalDiscountPrice = new Price(price);
+    }
+
+    public void setDiscountPrice(DiscountEvent discountEvent) {
+
+        int price = 0;
+
+        for (String title : discountEvent.getDiscountEventHistory().keySet()) {
+            if (title.equals(Event.GIVE_AWAY_EVENT.getTitle())) {
+                continue;
+            }
+            price += discountEvent.getDiscountEventHistory().get(title);
+        }
+
+        this.discountPrice = new Price(price);
+    }
+
+    public void setExpectedPaymentPrice() {
+        this.expectedPaymentPrice = new Price(totalPriceBeforeDiscount.getValue() - discountPrice.getValue());
     }
 
     public Price getTotalPriceBeforeDiscount() {
         return totalPriceBeforeDiscount;
+    }
+
+    public Price getTotalDiscountPrice() {
+        return totalDiscountPrice;
+    }
+
+    public Price getExpectedPaymentPrice() {
+        return expectedPaymentPrice;
     }
 }
