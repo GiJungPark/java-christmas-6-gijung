d0bad973ec19942a31aad0f95018059937ba9716
GiJung <weeds2577@gmail.com>
2023-11-15 16:30:51 +0900

    feat(VisitDate): 이벤트 날짜를 처리할 수 있는 메서드 추가

--- src/main/java/christmas/domain/VisitDate.java
+++ src/main/java/christmas/domain/VisitDate.java
@@ -2,6 +2,13 @@ package christmas.domain;
 
 public class VisitDate {
 
+    private static final int CHRISTMAS = 25;
+    private static final int WEEK = 7;
+    private static final int FRIDAY = 1;
+    private static final int SATURDAY = 2;
+    private static final int SUNDAY = 3;
+
+
     private final int number;
 
     public VisitDate(int number) {
@@ -19,5 +26,21 @@ public class VisitDate {
 
     public int getNumber() {
         return number;
+    }
+
+    public boolean isContainChristmas() {
+        return number == CHRISTMAS;
+    }
+
+    public boolean isContainWeekday() {
+        return (number % WEEK != FRIDAY && number % WEEK != SATURDAY);
+    }
+
+    public boolean isContainWeekend() {
+        return (number % WEEK == FRIDAY || number % WEEK == SATURDAY);
+    }
+
+    public boolean isContainSpecial() {
+        return (number % WEEK == SUNDAY || number == CHRISTMAS);
     }
 }
